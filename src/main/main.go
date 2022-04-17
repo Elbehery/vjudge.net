d276af2cf5266a30867498a0f040e86bf8e2f31e
elbehery <melbeher@redhat.com>
2022-04-17 16:17:24 +0200

    cards

--- src/main/main.go
+++ src/main/main.go
@@ -1,7 +1,98 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+type Card struct {
+	Term       string
+	Definition string
+}
+
+func (c *Card) Validate(answer string) bool {
+	return c.Definition == answer
+}
+
+func New(term, definition string) *Card {
+	return &Card{
+		Term:       term,
+		Definition: definition,
+	}
+}
 
 func main() {
-	fmt.Println("Hello Judge")
+
+	cards := []*Card{}
+	termsSet := map[string]bool{}
+	definitionSet := map[string]string{}
+	reader := bufio.NewReader(os.Stdin)
+	// read the number
+	var n int
+	fmt.Println("Input the number of cards:")
+	fmt.Scanln(&n)
+
+	i := 1
+	for i <= n {
+		// read the term
+		fmt.Printf("The term for card #%v:\n", i)
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+		term := strings.TrimSpace(line)
+		for termsSet[term] {
+			fmt.Printf("The term \"%v\" already exists. Try again:\n", term)
+			line, err := reader.ReadString('\n')
+			if err != nil {
+				log.Fatal(err)
+			}
+			term = strings.TrimSpace(line)
+		}
+		termsSet[term] = true
+
+		// read the definition
+		fmt.Printf("The definition for card #%v:\n", i)
+		line, err = reader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+		definition := strings.TrimSpace(line)
+		for definitionSet[definition] != "" {
+			fmt.Printf("The definition \"%v\" already exists. Try again:\n", definition)
+			line, err := reader.ReadString('\n')
+			if err != nil {
+				log.Fatal(err)
+			}
+			definition = strings.TrimSpace(line)
+		}
+		definitionSet[definition] = term
+
+		// create the card and append
+		card := New(term, definition)
+		cards = append(cards, card)
+
+		i++
+	}
+
+	for _, c := range cards {
+		fmt.Printf("Print the definition of \"%v\":\n", c.Term)
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+		answer := strings.TrimSpace(line)
+		if c.Validate(answer) {
+			fmt.Print("Correct!\n")
+		} else {
+			if t, ok := definitionSet[answer]; ok {
+				fmt.Printf("Wrong. The right answer is \"%v\", but your definition is correct for \"%v\".\n", c.Definition,t)
+			} else {
+				fmt.Printf("Wrong. The right answer is \"%v\".\n", c.Definition)
+			}
+		}
+	}
 }
--- src/string_processing_sheet/main.go
+++ src/string_processing_sheet/main.go
@@ -1,13 +1,75 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
-const str = "Hello Judge"
-
-func main() {
-	fmt.Println(run())
+type Cards struct {
+	data          map[string]string
+	definitionSet map[string]bool
 }
 
-func run() string {
-	return str
+func NewCards() *Cards {
+	return &Cards{
+		data:          make(map[string]string),
+		definitionSet: make(map[string]bool),
+	}
+}
+
+func (c *Cards) Add() {
+	// capture term from user
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("The card:")
+	term, _ := reader.ReadString('\n')
+	term = strings.TrimSpace(term)
+	for c.data[term] != "" {
+		fmt.Printf("The term \"%v\" already exists. Try again:\n", term)
+		line, _ := reader.ReadString('\n')
+		term = strings.TrimSpace(line)
+	}
+
+	// capture definition from user
+	fmt.Println("The definition of the card:")
+	definition, _ := reader.ReadString('\n')
+	definition = strings.TrimSpace(definition)
+	for c.definitionSet[definition] {
+		fmt.Printf("The definition \"%v\" already exists. Try again:\n", definition)
+		line, _ := reader.ReadString('\n')
+		definition = strings.TrimSpace(line)
+	}
+
+	// print msg to user
+	fmt.Println(ConfirmMsg(term, definition))
+	// add to indices
+	c.data[term] = definition
+	c.definitionSet[definition] = true
+}
+
+func ConfirmMsg(term, definition string) string {
+	return fmt.Sprintf("The pair (\"%v\":\"%v\") has been added.\n", term, definition)
+}
+
+func main() {
+
+	// validation data structure
+	cards := NewCards()
+
+	for {
+		//capture input cmd
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Println("Input the action (add, remove, import, export, ask, exit):")
+		input, _ := reader.ReadString('\n')
+		input = strings.TrimSpace(input)
+
+		switch input {
+		case "add":
+			cards.Add()
+		case "exit":
+			return
+		}
+	}
+
 }

cb33e2b65eea6a5ea4f463b507584cb76ff01cbf
elbehery <melbeher@redhat.com>
2022-04-17 18:10:36 +0200

    stage 4

--- src/string_processing_sheet/main.go
+++ src/string_processing_sheet/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -17,19 +19,6 @@ func NewCards() *Cards {
 		data:          make(map[string]string),
 		definitionSet: make(map[string]bool),
 	}
-}
-
-func (c *Cards) Remove() {
-	fmt.Println("Which card?")
-	reader := bufio.NewReader(os.Stdin)
-	input, _ := reader.ReadString('\n')
-	input = strings.TrimSpace(input)
-	if _, ok := c.data[input]; ok {
-		delete(c.data, input)
-		fmt.Println("The card has been removed.")
-		return
-	}
-	fmt.Printf("Can't remove \"%v\": there is no such card.\n", input)
 }
 
 func (c *Cards) Add() {
@@ -61,6 +50,108 @@ func (c *Cards) Add() {
 	c.definitionSet[definition] = true
 }
 
+func (c *Cards) Remove() {
+	fmt.Println("Which card?")
+	reader := bufio.NewReader(os.Stdin)
+	input, _ := reader.ReadString('\n')
+	input = strings.TrimSpace(input)
+	if _, ok := c.data[input]; ok {
+		delete(c.data, input)
+		fmt.Println("The card has been removed.")
+		return
+	}
+	fmt.Printf("Can't remove \"%v\": there is no such card.\n", input)
+}
+
+func (c *Cards) Import() {
+	fmt.Println("File name:")
+	reader := bufio.NewReader(os.Stdin)
+	input, _ := reader.ReadString('\n')
+	input = strings.TrimSpace(input)
+	f, err := os.OpenFile(input, os.O_RDONLY, 0444)
+	defer f.Close()
+	if err != nil {
+		fmt.Println("File not found.")
+		return
+	}
+	i := 0
+	r := bufio.NewScanner(f)
+	for r.Scan() {
+		card := r.Text()
+		pair := strings.Split(card, "\\s")
+		if len(pair) < 2 {
+			continue
+		}
+		i++
+		c.data[pair[0]] = pair[1]
+		c.definitionSet[pair[1]] = true
+	}
+	fmt.Printf("%v cards have been loaded\n", i)
+}
+
+func (c *Cards) Export() {
+	fmt.Println("File name:")
+	reader := bufio.NewReader(os.Stdin)
+	input, _ := reader.ReadString('\n')
+	input = strings.TrimSpace(input)
+
+	f, err := os.OpenFile(input, os.O_RDWR|os.O_APPEND|os.O_CREATE, 0666)
+	defer f.Close()
+	if err != nil {
+		fmt.Printf("error opening file %v \n", input)
+		return
+	}
+
+	f.WriteString("\n")
+	for k, v := range c.data {
+		f.WriteString(fmt.Sprintf("%v\\s%v\n", k, v))
+	}
+	fmt.Printf("%v cards have been saved.\n", len(c.data))
+}
+
+func (c *Cards) Ask() {
+	fmt.Println("How many times to ask?")
+	reader := bufio.NewReader(os.Stdin)
+	input, _ := reader.ReadString('\n')
+	input = strings.TrimSpace(input)
+	limit, err := strconv.Atoi(input)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// dump keys into slice
+	keys := make([]string, 0, len(c.data))
+	for k := range c.data {
+		keys = append(keys, k)
+	}
+
+	i := 0
+	idx := 0
+	for i < limit {
+
+		if idx == len(keys) {
+			idx = 0
+		}
+
+		k := keys[idx]
+		v := c.data[k]
+		fmt.Printf("Print the definition of \"%v\":\n", k)
+		input, _ = reader.ReadString('\n')
+		input = strings.TrimSpace(input)
+		if input == v {
+			fmt.Println("Correct!")
+		} else if t, ok := c.definitionSet[input]; ok {
+			fmt.Printf("Wrong. The right answer is \"%v\", but your definition is correct for \"%v\".\n", v, t)
+		} else {
+			fmt.Printf("Wrong. The right answer is \"%v\".\n", v)
+		}
+
+		idx++
+		i++
+	}
+
+}
+
 func ConfirmMsg(term, definition string) string {
 	return fmt.Sprintf("The pair (\"%v\":\"%v\") has been added.\n", term, definition)
 }
@@ -82,6 +173,12 @@ func main() {
 			cards.Add()
 		case "remove":
 			cards.Remove()
+		case "import":
+			cards.Import()
+		case "export":
+			cards.Export()
+		case "ask":
+			cards.Ask()
 		case "exit":
 			fmt.Println("Bye bye!")
 			return

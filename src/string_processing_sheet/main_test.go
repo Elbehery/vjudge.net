947d7ead3d11cf2a854d8f68a0dd1d79e2d8c07d
elbehery <melbeher@redhat.com>
2022-04-23 17:03:37 +0200

    resetting

--- src/string_processing_sheet/main_test.go
+++ src/string_processing_sheet/main_test.go
@@ -3,8 +3,5 @@ package main
 import "testing"
 
 func TestGreeting(t *testing.T) {
-	output := run()
-	if output != str {
-		t.Fatalf("incorrect greeting, expected %v, but got %v instead", str, output)
-	}
+
 }
